3a76c11c0a99311c61dfb00eab6a573fb2eebe33
Brittany Dowdle <162188958+Bdowdle4@users.noreply.github.com>
2024-03-10 16:35:52 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shinywidgets import render_plotly
+import palmerpenguins
+
+penguins_df = palmerpenguins.load_penguins()
+
+ui.page_opts(title="Brittany's Data About Spectacular Penguins", fillable=True)
+with ui.layout_columns():
+
+    @render_plotly
+    def plot1():
+        return px.histogram(px.data.tips(), y="tip")
+
+    @render_plotly
+    def plot2():
+        return px.histogram(px.data.tips(), y="total_bill")

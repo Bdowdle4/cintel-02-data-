13694e6886fdd8415698a10169a2d184dc48de75
Brittany Dowdle <162188958+Bdowdle4@users.noreply.github.com>
2024-03-10 16:59:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+# These are the packages you need for your histograms
 import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
-import palmerpenguins
+import palmerpenguins # This package provides the Palmer Penguins dataset
 
+# Get the data
+# ALWAYS familiarize yourself with the dataset you are working with first
+
+# Use the built-in function to load the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
 
+# Define User Interface (ui)
 ui.page_opts(title="Brittany's Data About Spectacular Penguins", fillable=True)
 with ui.layout_columns():
 
+#These will create the visuals for your histograms    
     @render_plotly
     def plot1():
         return px.histogram(px.data.tips(), y="tip")
